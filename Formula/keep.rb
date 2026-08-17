608bea1dfb9a67dcc07a642e559e22f73fe797bb
you guan <yixin.guan@longbridge-inc.com>
2026-08-17 18:36:13 +0800

    fix keep sha256 (repo made public)

--- Formula/keep.rb
+++ Formula/keep.rb
@@ -2,7 +2,7 @@ class Keep < Formula
   desc "Project-aware process supervisor for local development"
   homepage "https://github.com/youguanxinqing/keep"
   url "https://github.com/youguanxinqing/keep/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "7515bf959b73b956ceb967351c7e299cbb3668a53d35f9c770eb72e00d93ced6"
+  sha256 "3c3cdb6a6c3f133ba6195287e93aa0df0eac608c91ba51b1467a6a7cd4743ae0"
   license "MIT"
   head "https://github.com/youguanxinqing/keep.git", branch: "master"
 
