3330a4d83aa2fc2fece1dc2f71f688307b17a4ec
Guan <youguanxinqing@qq.com>
2026-08-25 11:18:51 +0800

    keep 0.2.1

--- Formula/keep.rb
+++ Formula/keep.rb
@@ -1,8 +1,8 @@
 class Keep < Formula
   desc "Project-aware process supervisor for local development"
   homepage "https://github.com/youguanxinqing/keep"
-  url "https://github.com/youguanxinqing/keep/releases/download/v0.2.0/keep-0.2.0.tar.gz"
-  sha256 "21ee1ebdc818434b29293f01025012d0d79992db4bb30c85215c0e5dee3e2a2d"
+  url "https://github.com/youguanxinqing/keep/releases/download/v0.2.1/keep-0.2.1.tar.gz"
+  sha256 "1cb58d9f62d08bfce7300180c6bfc481effc52a118dccd6bcce4c97cfab23a63"
   license "MIT"
   head "https://github.com/youguanxinqing/keep.git", branch: "master"
 
